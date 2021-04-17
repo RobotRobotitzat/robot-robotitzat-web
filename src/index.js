546940a551f99c21db7ac4949e487a764ebd7081
anbecodes <anbecodes@gmail.com>
2021-04-17 23:17:11 +0200

    index js file

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+// routes
+const indexRoutes = require('./routes/index.js');
+app.use(indexRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// listen port
+const port = app.get('port')
+app.listen(app.get('port'), () => {
+    console.log("Listen on port ${port}");
+});
