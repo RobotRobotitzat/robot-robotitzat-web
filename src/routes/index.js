755d68bbb74b61714a252f22346399cf7962a63f
anbecodes <anbecodes@gmail.com>
2021-04-18 00:29:55 +0200

    add a new line

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // home route
 router.get('/', (req, res) => {
-    res.render('index.html', { title: 'Robot Robotitzat'})
+    res.render('index.html', { title: 'Robot Robotitzat'});
 });
 
 module.exports = router;
