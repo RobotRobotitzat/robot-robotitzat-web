905e78b396c43344534d1c77d74f17b8a521b147
anbecodes <anbecodes@gmail.com>
2021-04-27 21:15:23 +0200

    change the name

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 // home route
 router.get('/', (req, res) => {
-    res.render('index.html', { title: 'Home - Robot Robotitzat'});
+    res.render('index.html', { title: 'Home - anbecodes web'});
 });
 
 // about route
 router.get('/about', (req, res) => {
-    res.render('about.html', { title: 'About Me - Robot Robotitzat'});
+    res.render('about.html', { title: 'About Me - anbecodes web'});
 })
 
 module.exports = router;
